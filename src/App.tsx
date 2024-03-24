5d50027090bf4e315ecce325fd07de66b9247989
Prabhu Kalyan Korivi <prabhukalyan30@gmail.com>
2024-03-24 22:25:55 +0530

    react counter app

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  let [counter, setCounter] = useState(0);
+
+  const incrementCount = () => {
+    setCounter(++counter);
+    console.log(`counter value : ${counter}`)
+  }
+  
+  const decrementCount = () => {
+    setCounter(--counter);
+    console.log(`counter value : ${counter}`)
+  }
+
+  return (
+    <>
+      <h1 className='heading'>React with Prabhu</h1>
+      <h3 className='counter-text'>Counter Value : <span className='counter-num'>{counter}</span> </h3>
+      <button className='btn' onClick={incrementCount}>Increment Count</button>
+      <button className='btn' onClick={() => decrementCount()}>Decrement Count</button>
+    </>
+  )
+}
+
+export default App
